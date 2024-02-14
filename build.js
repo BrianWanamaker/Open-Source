559b6ad1e6169ef98f67fee1b9201cda33d5bb15
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2024-02-14 17:57:16 -0500

    added build process

---
+++ build.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const replace = require("replace-in-file");
+
+const options = {
+  files: "path/to/your/firebaseConfig.js",
+  from: [
+    /__API_KEY__/g,
+    /__AUTH_DOMAIN__/g,
+    /__DATABASE_URL__/g,
+    /__PROJECT_ID__/g,
+    /__STORAGE_BUCKET__/g,
+    /__MESSAGING_SENDER_ID__/g,
+    /__APP_ID__/g,
+    /__MEASUREMENT_ID__/g,
+  ],
+  to: [
+    process.env.API_KEY,
+    process.env.AUTH_DOMAIN,
+    process.env.DATABASE_URL,
+    process.env.PROJECT_ID,
+    process.env.STORAGE_BUCKET,
+    process.env.MESSAGING_SENDER_ID,
+    process.env.APP_ID,
+    process.env.MEASUREMENT_ID,
+  ],
+};
+
+replace(options)
+  .then((results) => {
+    console.log("Replacement results:", results);
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: "__API_KEY__",
+  authDomain: "__AUTH_DOMAIN__",
+  databaseURL: "__DATABASE_URL__",
+  projectId: "__PROJECT_ID__",
+  storageBucket: "__STORAGE_BUCKET__",
+  messagingSenderId: "__MESSAGING_SENDER_ID__",
+  appId: "__APP_ID__",
+  measurementId: "__MEASUREMENT_ID__",
 };
+firebase.initializeApp(firebaseConfig);
