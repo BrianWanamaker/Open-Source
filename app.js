3ce7e891996ddb51b8c8f6da5e72dee1c0919f59
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2024-03-18 17:23:26 -0400

    changed kick key

--- app.js
+++ app.js
@@ -128,8 +128,6 @@ function getRandomSafeSpot() {
   let coffeeX = 7; // Initial x position
   let coffeeY = 7; // Initial y position
 
-
-
   const gameContainer = document.querySelector(".game-container");
   const playerNameInput = document.querySelector("#player-name");
   const playerColorButton = document.querySelector("#player-color");
@@ -460,13 +458,7 @@ function getRandomSafeSpot() {
     }
   }
 
-
-
-
   function initGame() {
-
-    
-
     new KeyPressListener("ArrowUp", () => handleArrowPress(0, -1));
     new KeyPressListener("ArrowDown", () => handleArrowPress(0, 1));
     new KeyPressListener("ArrowLeft", () => handleArrowPress(-1, 0));
@@ -491,15 +483,8 @@ function getRandomSafeSpot() {
         const left = 16 * characterState.x + "px";
         const top = 16 * characterState.y - 4 + "px";
         el.style.transform = `translate3d(${left}, ${top}, 0)`;
-
       });
     });
-
-
-
-
-
-
 
     allPlayersRef.on("child_added", (snapshot) => {
       //Fires whenever a new node is added the tree
@@ -529,26 +514,33 @@ function getRandomSafeSpot() {
         addedPlayer.coins;
       characterElement.setAttribute("data-color", addedPlayer.color);
       characterElement.setAttribute("data-direction", addedPlayer.direction);
-      characterElement.setAttribute("data-animation-state", addedPlayer.animationState);
+      characterElement.setAttribute(
+        "data-animation-state",
+        addedPlayer.animationState
+      );
       const left = 16 * addedPlayer.x + "px";
       const top = 16 * addedPlayer.y - 4 + "px";
       characterElement.style.transform = `translate3d(${left}, ${top}, 0)`;
       gameContainer.appendChild(characterElement);
     });
 
-    
     let movementFlags = {
       up: false,
       down: false,
       left: false,
       right: false,
     };
-    
+
     // Function to check if any movement flag is true
     function isMoving() {
-      return movementFlags.up || movementFlags.down || movementFlags.left || movementFlags.right;
+      return (
+        movementFlags.up ||
+        movementFlags.down ||
+        movementFlags.left ||
+        movementFlags.right
+      );
     }
-    
+
     // Update the animation state based on movement
     function updateAnimationState() {
       if (isMoving()) {
@@ -563,118 +555,131 @@ function getRandomSafeSpot() {
         });
       }
     }
-    
+
     document.addEventListener("keydown", function (event) {
       let keyHandled = false;
       if (event.key === "ArrowUp") {
-
         movementFlags.up = true;
         keyHandled = true;
       } else if (event.key === "ArrowDown") {
-
         movementFlags.down = true;
         keyHandled = true;
       } else if (event.key === "ArrowLeft") {
-
         movementFlags.left = true;
         keyHandled = true;
       } else if (event.key === "ArrowRight") {
-        
         movementFlags.right = true;
         keyHandled = true;
-      }  else if (event.key === " " || event.code === "Space") { // Detect space bar
+      } else if (event.key === "k") {
+        // Detect space bar
         performKick();
         playerRef.update({
           animationState: "kick", // Update the player's animation state to "kick"
-          
         });
-       
+
         event.preventDefault(); // Prevent the default space bar action (e.g., page scrolling)
         return; // Skip the movementFlags check and update
-      }else if (event.key === "p"){
+      } else if (event.key === "p") {
         attemptToPickUpPlayer();
-      }else if (event.key === "t"){
+      } else if (event.key === "t") {
         attemptToThrowPlayer();
       }
-    
+
       if (keyHandled) {
         event.preventDefault(); // Prevent default action (e.g., scrolling the page)
         updateAnimationState(); // Update the animation state based on the current movement
       }
     });
-    
-    
-    
+
     document.addEventListener("keyup", function (event) {
       if (event.key === "ArrowUp") movementFlags.up = false;
       else if (event.key === "ArrowDown") movementFlags.down = false;
       else if (event.key === "ArrowLeft") movementFlags.left = false;
       else if (event.key === "ArrowRight") movementFlags.right = false;
-    
+
       updateAnimationState(); // Update the animation state when keys are released
     });
-    
+
     function performKick() {
       const kickDirection = players[playerId].direction;
-      const kickRangeX = players[playerId].x + (kickDirection === "right" ? 1 : kickDirection === "left" ? -1 : 0);
-      const kickRangeY = players[playerId].y + (kickDirection === "up" ? -1 : kickDirection === "down" ? 1 : 0);
-    
-      Object.keys(players).forEach(function(id) {
+      const kickRangeX =
+        players[playerId].x +
+        (kickDirection === "right" ? 1 : kickDirection === "left" ? -1 : 0);
+      const kickRangeY =
+        players[playerId].y +
+        (kickDirection === "up" ? -1 : kickDirection === "down" ? 1 : 0);
+
+      Object.keys(players).forEach(function (id) {
         if (players[id].x === kickRangeX && players[id].y === kickRangeY) {
           // Attempt to bump the player back by 2 spaces, but check for obstacles
           for (let i = 2; i > 0; i--) {
-            const bumpDirectionMultiplier = kickDirection === "right" ? i : kickDirection === "left" ? -i : 0;
+            const bumpDirectionMultiplier =
+              kickDirection === "right" ? i : kickDirection === "left" ? -i : 0;
             const bumpedBackX = players[id].x + bumpDirectionMultiplier;
-            const bumpVerticalMultiplier = kickDirection === "down" ? i : kickDirection === "up" ? -i : 0;
+            const bumpVerticalMultiplier =
+              kickDirection === "down" ? i : kickDirection === "up" ? -i : 0;
             const bumpedBackY = players[id].y + bumpVerticalMultiplier;
-    
+
             // Check if the spot is free or if it's within boundaries
-            if (!isSolid(bumpedBackX, bumpedBackY) && withinBoundaries(bumpedBackX, bumpedBackY)) {
+            if (
+              !isSolid(bumpedBackX, bumpedBackY) &&
+              withinBoundaries(bumpedBackX, bumpedBackY)
+            ) {
               // Move the player as far as possible before hitting the obstacle
               // Update the bumped player's position and animation state to "hit" in Firebase
               firebase.database().ref(`players/${id}`).update({
                 x: bumpedBackX,
                 y: bumpedBackY,
-                animationState: "hit" // Set the animation state to "hit"
+                animationState: "hit", // Set the animation state to "hit"
               });
-              console.log(`Player ${id} was kicked and moved to (${bumpedBackX}, ${bumpedBackY}).`);
-    
+              console.log(
+                `Player ${id} was kicked and moved to (${bumpedBackX}, ${bumpedBackY}).`
+              );
+
               // Set a timeout to revert the animation state after 1 second
               setTimeout(() => {
                 firebase.database().ref(`players/${id}`).update({
-                  animationState: "idle" // Revert to idle or another appropriate state
+                  animationState: "idle", // Revert to idle or another appropriate state
                 });
               }, 500); // 1000 milliseconds = 1 second
-    
+
               break; // Exit the loop after moving the player
             }
           }
         }
       });
     }
-    
-    
+
     function attemptToPickUpPlayer() {
-      Object.keys(players).forEach(function(id) {
-        if (id !== playerId) { // Make sure the player is not trying to pick up themselves
+      Object.keys(players).forEach(function (id) {
+        if (id !== playerId) {
+          // Make sure the player is not trying to pick up themselves
           const otherPlayer = players[id];
           const distanceX = Math.abs(players[playerId].x - otherPlayer.x);
           const distanceY = Math.abs(players[playerId].y - otherPlayer.y);
-          
+
           // Check if the other player is adjacent (one tile away in any direction)
-          if ((distanceX === 1 && distanceY === 0) || (distanceX === 0 && distanceY === 1)) {
+          if (
+            (distanceX === 1 && distanceY === 0) ||
+            (distanceX === 0 && distanceY === 1)
+          ) {
             carryingPlayerId = id; // Set the carrying state
-            
-          
-            firebase.database().ref(`players/${playerId}`).update({carrying: id});
-            firebase.database().ref(`players/${id}`).update({isCarried: true});
-    
+
+            firebase
+              .database()
+              .ref(`players/${playerId}`)
+              .update({ carrying: id });
+            firebase
+              .database()
+              .ref(`players/${id}`)
+              .update({ isCarried: true });
+
             console.log(`Player ${playerId} is now carrying player ${id}.`);
           }
         }
       });
     }
-    
+
     const throwDistance = 2; // Tiles the player can be thrown
 
     function attemptToThrowPlayer() {
@@ -682,54 +687,60 @@ function getRandomSafeSpot() {
         const direction = players[playerId].direction; // The carrying player's facing direction
         let newX = players[playerId].x; // Start from the carrying player's position
         let newY = players[playerId].y;
-    
+
         // Adjust the thrown player's new position based on the direction
         switch (direction) {
-          case 'up':
+          case "up":
             newY -= throwDistance; // Move up relative to the carrying player
             break;
-          case 'down':
+          case "down":
             newY += throwDistance; // Move down
             break;
-          case 'left':
+          case "left":
             newX -= throwDistance; // Move left
             break;
-          case 'right':
+          case "right":
             newX += throwDistance; // Move right
             break;
           default:
             console.log("Unknown direction");
             return; // Exit if direction is not recognized
         }
-    
+
         // Validate the new position
         if (!isSolid(newX, newY) && withinBoundaries(newX, newY)) {
           // Update the thrown player's position
           players[carryingPlayerId].x = newX;
           players[carryingPlayerId].y = newY;
-          
+
           // Update Firebase with the new position of the thrown player
           firebase.database().ref(`players/${carryingPlayerId}`).update({
             x: newX,
             y: newY,
-            isCarried: false // Reset the carried status
+            isCarried: false, // Reset the carried status
           });
-    
+
           // Reset the carrying player's state
-          firebase.database().ref(`players/${playerId}`).update({carrying: null});
+          firebase
+            .database()
+            .ref(`players/${playerId}`)
+            .update({ carrying: null });
           carryingPlayerId = null; // No longer carrying
         } else {
           console.log("Invalid throw destination.");
         }
       }
     }
-    
-    function withinBoundaries(x, y) {
-      return x >= mapData.minX && x <= mapData.maxX && y >= mapData.minY && y <= mapData.maxY;
-    }
-    
 
-  
+    function withinBoundaries(x, y) {
+      return (
+        x >= mapData.minX &&
+        x <= mapData.maxX &&
+        y >= mapData.minY &&
+        y <= mapData.maxY
+      );
+    }
+
     //Remove character DOM element after they leave
     allPlayersRef.on("child_removed", (snapshot) => {
       const removedKey = snapshot.val().id;
@@ -838,7 +849,6 @@ function getRandomSafeSpot() {
     //Place NPC
 
     placeAndMoveNPC();
-
   }
 
   firebase.auth().onAuthStateChanged((user) => {
