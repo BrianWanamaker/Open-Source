548dc00f9153dc22845018f840827e7456ecef70
BrianWanamaker <bcwanamaker@quinnipiac.edu>
2024-04-28 19:08:52 -0400

    added escape to welcome screen

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import { placeCoin } from "./src/coins.js";
 import { chat } from "./src/chat.js";
 
 export function initGame() {
-  firebase.database().ref('gameState').set({
+  firebase.database().ref("gameState").set({
     gameEnded: false,
-    winnerId: null
+    winnerId: null,
   });
 }
